e4e2d9631dba84efbf18351fd5be892b7a58a095
Daniel <danielkibret8@gmail.com>
2025-05-09 22:47:12 +0300

    About completed

--- src/Components/JoinNFT.tsx
+++ src/Components/JoinNFT.tsx
@@ -5,10 +5,10 @@ const JoinNFT : React.FC = ()=>{
   return(
     <section className='w-full h-screen'>
       <div className='w-full h-2/6 '>
-        <span className='mx-1/2 ont-["Russo_One"] p-2 bg-gradient-to-r from-[#703DFA]/60 to-[#6c54ad] bg-clip-text text-transparent 
-              rounded-3xl bg-[#e9e4f8] text-center '>
+        <h2 className='font-["Russo_One"] p-2 bg-gradient-to-r from-[#703DFA]/60 to-[#6c54ad] bg-clip-text text-transparent 
+             rounded-3xl bg-[#e9e4f8] text-center'>
           Join NFT Portfolio
-        </span>
+        </h2>
         <h1 className='text-white font-["Russo_One"] text-center text-5xl'>
           Become a Mintora <br /> Player Now
         </h1>
